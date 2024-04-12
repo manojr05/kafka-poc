421512d847675b22369746a1bfcf5ccf2ba4642c
Manoj R <manoj.r@diatoz.com>
2024-04-12 14:44:39 +0530

    added the ports to the modules

--- kafkaconsumer/src/main/java/com/kafkaconsumer/KafkaConsumerApplication.java
+++ kafkaconsumer/src/main/java/com/kafkaconsumer/KafkaConsumerApplication.java
@@ -1,7 +1,11 @@
 package com.kafkaconsumer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class KafkaConsumerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(KafkaConsumerApplication.class, args);
     }
 }
