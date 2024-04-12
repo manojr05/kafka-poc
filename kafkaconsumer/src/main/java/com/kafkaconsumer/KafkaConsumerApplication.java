a88f9781c67f66bd63c5dd86e24e2ee6da369c7b
Manoj R <manoj.r@diatoz.com>
2024-04-12 14:41:15 +0530

    added two modules to the project

---
+++ kafkaconsumer/src/main/java/com/kafkaconsumer/KafkaConsumerApplication.java
@@ -0,0 +1,7 @@
+package com.kafkaconsumer;
+
+public class KafkaConsumerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ kafkaproducer/src/main/java/com/kafkaproducer/KafkaProducerApplication.java
@@ -0,0 +1,7 @@
+package com.kafkaproducer;
+
+public class KafkaProducerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
